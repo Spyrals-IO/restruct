080f5827baa2cef97b5d5015d6c28fb2011f573b
merlin goulet <mgo@zengularity.com>
2018-12-26 14:36:31 +0100

    simple query string tests and corrections

--- queryStringBindable/src/main/scala/io/github/methrat0n/restruct/handlers/queryStringBindable/queryStringBindable.scala
+++ queryStringBindable/src/main/scala/io/github/methrat0n/restruct/handlers/queryStringBindable/queryStringBindable.scala
@@ -1,9 +1,8 @@
 package io.github.methrat0n.restruct.handlers.queryStringBindable
 
-import io.github.methrat0n.restruct.core.Program
-import io.github.methrat0n.restruct.core.data.schema.SimpleSchemaAlgebra
+import io.github.methrat0n.restruct.schema.Schema
 import play.api.mvc.QueryStringBindable
 
 object queryStringBindable extends SimpleQueryStringBindableInterpreter with ComplexQueryStringBindableInterpreter with FieldQueryStringBindableInterpreter {
-  def run[T](program: Program[SimpleSchemaAlgebra, T]): QueryStringBindable[T] = program.run(this)
+  def run[T](program: Schema[T]): QueryStringBindable[T] = program.bind(this)
 }
