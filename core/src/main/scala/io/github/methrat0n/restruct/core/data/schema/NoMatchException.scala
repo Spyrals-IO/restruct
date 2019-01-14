dbb6d3d46714d1615b90acce4ffb9f99e9d9c3d0
Methrat0n <merlingoulet@live.fr>
2019-01-14 14:07:41 +0100

    remove specific error message in generic case

--- core/src/main/scala/io/github/methrat0n/restruct/core/data/schema/NoMatchException.scala
+++ core/src/main/scala/io/github/methrat0n/restruct/core/data/schema/NoMatchException.scala
@@ -10,12 +10,12 @@ object NoMatchException {
   def product(e1: Throwable, e2: Throwable): NoMatchException =
     (e1, e2) match {
       case (aNoMatch: NoMatchException, bNoMatch: NoMatchException) =>
-        throw new NoMatchException("Cannot find value in bson", (aNoMatch.errors ++ bNoMatch.errors): _*)
+        throw new NoMatchException("Cannot find value", (aNoMatch.errors ++ bNoMatch.errors): _*)
       case (aNoMatch: NoMatchException, _) =>
-        throw new NoMatchException("Cannot find value in bson", (aNoMatch.errors :+ e2): _*)
+        throw new NoMatchException("Cannot find value", (aNoMatch.errors :+ e2): _*)
       case (_, bNoMatch: NoMatchException) =>
-        throw new NoMatchException("Cannot find value in bson", (bNoMatch.errors :+ e1): _*)
+        throw new NoMatchException("Cannot find value", (bNoMatch.errors :+ e1): _*)
       case _ =>
-        throw new NoMatchException("Cannot find value in bson", e1, e2)
+        throw new NoMatchException("Cannot find value", e1, e2)
     }
 }
