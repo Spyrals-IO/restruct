73aa368f1bcf1e39e8ac867928ac851fba23fab9
Methrat0n <merlingoulet@live.fr>
2019-02-17 19:20:02 +0100

    bson fields test and fixes

--- bson/reader/src/main/scala/io/github/methrat0n/restruct/readers/bson/package.scala
+++ bson/reader/src/main/scala/io/github/methrat0n/restruct/readers/bson/package.scala
@@ -1,7 +1,7 @@
 package io.github.methrat0n.restruct.readers
 
 import io.github.methrat0n.restruct.core.data.schema.{ IntStep, Path, StringStep }
-import reactivemongo.bson.{ BSONDocument, BSONReader, BSONValue }
+import reactivemongo.bson.{ BSONArray, BSONDocument, BSONElementSet, BSONReader, BSONValue }
 
 package object bson {
   type BsonReader[T] = BSONReader[BSONValue, T]
@@ -11,26 +11,31 @@ package object bson {
   }
 
   private[readers] def readDocumentWithDefault[T](path: Path, schema: BsonReader[T], default: Option[T]): BsonReader[Option[T]] = BsonReader[Option[T]] {
-    case document: BSONDocument => default
-      .map(defaultValue => Some(getAs(path, document, schema).getOrElse(defaultValue)))
-      .getOrElse(getAs(path, document, schema))
-    //(schema)
+    case bson: BSONValue with BSONElementSet => default
+      .map(defaultValue => Some(getAs(path, bson, schema).getOrElse(defaultValue)))
+      .getOrElse(getAs(path, bson, schema))
     case value => throw new RuntimeException(s"bsonvalue $value should be a BSONDocument")
   }
 
-  private[this] def getAs[T](path: Path, document: BSONDocument, schema: BsonReader[T]): Option[T] =
-    path.steps.toList.foldLeft(document.get(path.steps.head match {
-      case StringStep(name) => name
-      case IntStep(index)   => index.toString
-    }))(
+  private[this] def getAs[T](path: Path, bson: BSONElementSet, schema: BsonReader[T]): Option[T] =
+    path.steps.toList.tail.foldLeft(path.steps.head match {
+      case StringStep(name) => bson match {
+        case document: BSONDocument => document.get(name)
+        case _                      => throw new RuntimeException(s"invalid path : ${path.steps.toList}")
+      }
+      case IntStep(index) => bson match {
+        case arr: BSONArray => arr.get(index.toString)
+        case _              => throw new RuntimeException(s"invalid path : ${path.steps.toList}")
+      }
+    })(
       (acc, step) => step match {
         case StringStep(name) => acc.flatMap {
           case document: BSONDocument => document.get(name)
           case _                      => throw new RuntimeException(s"invalid path : ${path.steps.toList}")
         }
         case IntStep(index) => acc.flatMap {
-          case document: BSONDocument => document.get(index.toString)
-          case _                      => throw new RuntimeException(s"invalid path : ${path.steps.toList}")
+          case arr: BSONArray => arr.get(index.toString)
+          case _              => throw new RuntimeException(s"invalid path : ${path.steps.toList}")
         }
       }
     ).map(_.as(schema))
