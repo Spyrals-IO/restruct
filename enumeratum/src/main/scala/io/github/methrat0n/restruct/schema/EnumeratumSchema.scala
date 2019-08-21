1e14ab971fa237549f61dd434d164e2450b9dd51
Methrat0n <merlingoulet@live.fr>
2019-08-21 12:35:20 +0200

    remove jsonSchema as the model is broken, we'll get it back later. Migrate the Enumeratum helper and add scala doc with example

--- enumeratum/src/main/scala/io/github/methrat0n/restruct/schema/EnumeratumSchema.scala
+++ enumeratum/src/main/scala/io/github/methrat0n/restruct/schema/EnumeratumSchema.scala
@@ -1,14 +1,43 @@
 package io.github.methrat0n.restruct.schema
 
-import enumeratum.{ Enum, EnumEntry }
+import enumeratum.{Enum, EnumEntry}
 import io.github.methrat0n.restruct.constraints.EnumeratumConstraints.EnumConstraint
-import io.github.methrat0n.restruct.core.data.schema.FieldAlgebra
+import io.github.methrat0n.restruct.schema.Interpreter.{ConstrainedInterpreter, InvariantInterpreter, SimpleInterpreter}
 
 import scala.language.higherKinds
 
-object EnumeratumSchema {
-  def enum[E <: EnumEntry](enum: Enum[E]): Schema[E] = new Schema[E] {
-    override def bind[FORMAT[_]](algebra: FieldAlgebra[FORMAT]): FORMAT[E] =
-      algebra.imap(algebra.verifying(algebra.stringSchema, EnumConstraint(enum)))(enum.withName)(_.entryName)
-  }
+/**
+  * In the companion object of your Enumeration you can use this helper to define the Schema of the Enumeration.
+  *
+  * {{{
+  *   import io.github.methrat0n.restruct.schema.EnumeratumSchema
+  *
+  *   sealed trait Cats extends EnumEntry
+  *
+  *   object Cats extends Enum[Cats] {
+  *
+  *     val values = findValues
+  *
+  *     case object StrayCats extends Cats
+  *     case object WhiteCats extends Cats
+  *     case object TortoiseShellCats extends Cats
+  *
+  *     implicit schema = EnumSchema(Cats) //pass the companion object itself
+  *   }
+  * }}}
+  *
+  * Notice : It will only work for Format that are Invariant and Constrainable.
+  */
+final case class EnumeratumSchema[E <: EnumEntry, InternalInterpreter[Format[_]] <: SimpleInterpreter[Format, String]](
+  enum: Enum[E]
+) extends Schema[E, λ[Format[_] => InvariantInterpreter[Format, String, E, ConstrainedInterpreter[Format, String, InternalInterpreter[Format]]]]] {
+  override def bind[Format[_]](implicit
+    interpreter: InvariantInterpreter[Format, String, E, ConstrainedInterpreter[Format, String, InternalInterpreter[Format]]]
+  ): Format[E] =
+    interpreter.imap(
+      interpreter.underlyingInterpreter.verifying(
+        interpreter.underlyingInterpreter.originalInterpreter.schema,
+        EnumConstraint(enum)
+      )
+    )(enum.withName)(_.entryName)
 }
--- jsonSchema/src/main/scala/io/github/methrat0n/restruct/writers/jsonschema/jsonSchema.scala
+++
@@ -1,7 +0,0 @@
-package io.github.methrat0n.restruct.writers.jsonschema
-
-import play.api.libs.json.{ JsObject, Writes }
-
-final case class JsonSchemaWriter[A](json: JsObject, private[writers] val writer: Writes[A])
-
-object jsonSchema extends SimpleJsonSchemaWriterInterpreter with ComplexJsonSchemaWriterInterpreter with FieldJsonSchemaWriterInterpreter
